d34b11addbe990b692398f8fc59c2aef34995a00
Aaron Michaels <aaronmichaels@gmail.com>
2016-11-01 00:11:12 -0400

    rename config to constants

--- ApiServer/config.js
+++
@@ -1,6 +0,0 @@
-//this file holds local application configuration
-//most of this config should be set in environemnt variables in production
-
-module.exports = {
-  JWT_SECRET: 'Pennlabs'
-}
---
+++ ApiServer/constants.js
@@ -0,0 +1,8 @@
+//this file holds local application constants
+//most of this config should be set in environemnt variables in production
+
+module.exports = {
+  JWT_SECRET: 'Pennlabs',
+  MONGO_URI: 'mongodb://localhost:apiserver/apiserver',
+  PORT: 3090
+}
