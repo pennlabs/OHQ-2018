dba381220ec0e049c4f40601a219fcccac0107ae
Julia Chun <hjchun96@gmail.com>
2016-11-17 22:13:48 -0500

    added potential routes and links to them on homepage

--- frontend/src/components/app.react.js
+++ frontend/src/components/app.react.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-import CommentBox from './CommentBox.react'
 import Header from './Header.react'
 
 export default class App extends Component {
@@ -12,7 +11,10 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <Link to='/signin'>Test Link</Link>
+        <div> This will be the splash page</div>
+        <Link to='signin'> Log In </Link><br/>
+        <Link to='class'> Class - main view </Link><br/>
+        <Link to='class/queue'> Queue </Link><br/>
         {this.props.children}
       </div>
     )
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,10 +3,16 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from './components/app.react'
 import Signin from './components/auth/Signin.react'
+import SearchBox from './components/SearchBox.react'
+import Queue from './components/Queue.react'
 
 export default (
 	<Route path='/' component={App}>
 		<Route path='/signin' component={Signin}>
+		</Route>
+		<Route path='/class' component={SearchBox}>
+		</Route>
+		<Route path='class/queue' component={Queue}>
 		</Route>
 	</Route>
 )
