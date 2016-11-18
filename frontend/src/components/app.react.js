47c77c05b2c641ae83be2dabf1f47b2de683468c
Aaron Michaels <aaronmichaels@gmail.com>
2016-11-18 17:00:20 -0500

    App component update

--- frontend/src/components/app.react.js
+++ frontend/src/components/app.react.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
+// import { Link, Router } from 'react-router'
+import { connect } from 'react-redux'
 
-import CommentBox from './CommentBox.react'
-import Header from './Header.react'
+// import CommentBox from './CommentBox.react'
+import SplashPage from './SplashPage.react'
+import Main from './Main.react'
 
-export default class App extends Component {
+class App extends Component {
+
+  renderMain() {
+    if (!this.props.authenticated) {
+      return <SplashPage />
+    }
+    return <Main>{this.props.children}</Main>
+  }
+
   render() {
     (async () => {
       await console.log('es8!')
     })()
     return (
-      <div>
-        <Header />
-        <Link to='/signin'>Test Link</Link>
-        {this.props.children}
-      </div>
+      this.renderMain()
     )
   }
 }
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+export default connect(mapStateToProps)(App)
