36080c885e85f9bfd76c116e419787d892f3ce31
Aaron Michaels <aaronmichaels@gmail.com>
2016-11-19 14:03:44 -0500

    Update sidebar

--- frontend/src/components/SideBar.react.js
+++ frontend/src/components/SideBar.react.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
 
 import styles from './../../style/sidebar.scss'
+import SidebarItem from './SidebarItem.react'
 
 class SideBar extends Component {
+
+  renderLinks() {
+
+  }
 
   render() {
     return (
       <nav className={styles.sidebar}>
+        <div className={styles.sidebarTitle}>
+          OHQ
+        </div>
         <div className={styles.sidebarLinks}>
-          <div>Link 1</div>
-          <div>Link 2</div>
-          <div>Link 3</div>
-          <div>Link 4</div>
-          <div>Link 5</div>
-          <div>Link 6</div>
-          <div>Link 7</div>
-          <div>Link 8</div>
+          <SidebarItem title='link 1'/>
+          <SidebarItem title='link 2' isActive isSelected/>
+          <SidebarItem title='link 3' isActive/>
+          <SidebarItem title='link 4'/>
+          <SidebarItem title='link 5'/>
+          <SidebarItem title='link 6'/>
+        </div>
+        <div className={styles.sidebarFooter}>
+          <span className={styles.plus}>+</span> Add classes
         </div>
       </nav>
     )
   }
+}
+
+SideBar.propTypes = {
+  classes: React.PropTypes.array,
 }
 
 export default SideBar
