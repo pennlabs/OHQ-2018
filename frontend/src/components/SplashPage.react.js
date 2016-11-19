72a57e7a8c6bd3494577276de410186f05c2703c
sknop8 <sknop8@gmail.com>
2016-11-18 20:25:18 -0500

    splash page impl

--- frontend/src/components/SplashPage.react.js
+++ frontend/src/components/SplashPage.react.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
+import { Link } from 'react-router'
+
+
+import styles from './../../style/splashpage.scss'
+
 class SplashPage extends Component {
 
   render() {
     return (
-      <div>
-        I am a splash page
+      <div className={styles.splashpage}>
+        <div className={styles.signin} >
+        	<div className={styles.title}>
+        		Office Hours Queue
+        	</div>
+        		
+    		<Link className={styles.button} to=''>SIGN IN</Link>
+        	
+
+        </div>
       </div>
     )
   }
