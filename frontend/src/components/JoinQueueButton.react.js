63e6575f17a66900beafaef766eae4fd07ba5933
Aaron Michaels <aaronmichaels@gmail.com>
2016-11-21 10:28:56 -0500

    Add color effect to join queue button
    Join queue button will now be dark green when the user depresses the mouse on it

--- frontend/src/components/JoinQueueButton.react.js
+++ frontend/src/components/JoinQueueButton.react.js
@@ -5,11 +5,35 @@ import styles from './../../style/joinqueuebutton.scss'
 
 class JoinQueueButton extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMouseDown: false,
+    }
+  }
+
+  getClassName() {
+    return this.state.isMouseDown
+    ? `${styles.greenBox} ${styles.mouseDown}`
+    : styles.greenBox
+  }
+
+  setMouseDownState = (state) => {
+    return (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      this.setState({isMouseDown: state})
+    }
+  }
+
   render() {
     return (
       <div
-        className={styles.greenBox}
+        className={this.getClassName()}
         title='Join the queue'
+        onMouseDown={this.setMouseDownState(true)}
+        onMouseUp={this.setMouseDownState(false)}
+        onMouseLeave={this.setMouseDownState(false)}
       >
         <CustomCross
           customClassName={styles.crossContainer}
