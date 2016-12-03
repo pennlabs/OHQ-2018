ed8e1112e46304414634c73275d423049b1450c2
Aaron Michaels <aaronmichaels@gmail.com>
2016-12-03 16:42:11 -0500

    code style + css fix

--- frontend/src/components/SplashPage.react.js
+++ frontend/src/components/SplashPage.react.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-
 import { Link } from 'react-router'
-
 
 import styles from './../../style/splashpage.scss'
 
@@ -13,8 +11,9 @@ class SplashPage extends Component {
         <img className={styles.logo} src={'https://cdn.zeplin.io/5827a5a8096f25bc1a22dab7/assets/1062CC45-9B56-46DA-BA4F-3B35C0AB32CE.png'} />
         <Link className={styles.button} to=''>Log In with PennKey</Link>
          <div
-           className={styles.labs}>
-           <span>Developed by<br/></span>
+           className={styles.labs}
+          >
+           <p>Developed by</p>
            <img
             styles={styles.labsImg}
             src={'https://cdn.zeplin.io/5827a5a8096f25bc1a22dab7/assets/91F41CD0-1042-429B-A05B-FD0AE7C7B3A2.png'}
