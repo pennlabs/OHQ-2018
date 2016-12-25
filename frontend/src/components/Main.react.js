1f94463bafc6d0a48266a5344c01b5b4ce0a2807
Aaron Michaels <aaronmichaels@gmail.com>
2016-12-24 20:37:26 -0700

    Naming update

--- frontend/src/components/Main.react.js
+++ frontend/src/components/Main.react.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-
 import styles from './../../style/main.scss'
-import SideBar from './SideBar.react'
+import Sidebar from './Sidebar.react'
 import ClassPage from './ClassPage.react'
 
 class Main extends Component {
@@ -18,7 +17,7 @@ class Main extends Component {
     ]
     return (
       <div className={styles.container}>
-				<SideBar classes={classes}/>
+				<Sidebar classes={classes} isTAForCurrentClass/>
         <ClassPage />
       </div>
     )
