ef6ac6c6893a57e166709298943c9386afce8221
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-10 18:21:03 -0500

    Fix typo

--- frontend/src/sockets/socketActionTypes.js
+++ frontend/src/sockets/socketActionTypes.js
@@ -2,5 +2,5 @@
 //actions that emit to the server have a corresponding return action.
 //Note that reducers generally only need to handle the return action.
 
-export const UPDATE_QUEUE = 'QUEUE_UPDATED'
+export const UPDATE_QUEUE = 'UPDATE_QUEUE'
 export const QUEUE_UPDATED = 'QUEUE_UPDATED'
