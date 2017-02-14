a1330d42db1c197362d58eed6c451ab22fa9c735
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-14 13:30:25 -0500

    Update class placeholders with names

--- frontend/src/components/Main.react.js
+++ frontend/src/components/Main.react.js
@@ -9,11 +9,11 @@ class Main extends Component {
 
   render() {
     const classes = [
-      {title: 'link 1'},
-      {title: 'link 2', isActive: true, isSelected: true},
-      {title: 'link 3', isActive: true},
-      {title: 'link 4'},
-      {title: 'link 5'},
+      {title: 'PHIL 473'},
+      {title: 'CIS 110', isActive: true, isSelected: true},
+      {title: 'URBS 243', isActive: true},
+      {title: 'WRIT 030'},
+      {title: 'MATH 455'},
     ]
     return (
       <div className={styles.container}>
