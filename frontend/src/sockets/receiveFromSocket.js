29ec3ee12299891f564d491259608437950de555
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 14:24:49 -0500

    update receiveFromSocket file

--- frontend/src/sockets/receiveFromSocket.js
+++ frontend/src/sockets/receiveFromSocket.js
@@ -3,8 +3,28 @@ import Socket from './initSocket.js'
 import { Store } from './../index.js'
 import * as SocketActions from './socketActionTypes'
 
-Socket.on('QUEUE_UPDATED', data => {
-  console.log('frontend received!')
-  console.log(data)
-  Store.dispatch({type: SocketActions.QUEUE_UPDATED, payload: data})
+Socket.on(SocketActions.CLASS_UPDATED, classData => {
+  console.log('class updated', classData)
+  Store.dispatch({type: SocketActions.CLASS_UPDATED, payload: classData})
+})
+
+//while the action is class queue updated, we're returning the entire class object.
+Socket.on(SocketActions.CLASS_QUEUE_UPDATED, classData => {
+  console.log('class queue updated', classData)
+  Store.dispatch({type: SocketActions.CLASS_QUEUE_UPDATED, payload: classData})
+})
+
+Socket.on(SocketActions.SELECTED_CLASS_UPDATED, classData => {
+  console.log('class updated', classData)
+  Store.dispatch({type: SocketActions.SELECTED_CLASS_UPDATED, payload: classData})
+})
+
+Socket.on(SocketActions.USER_INFO_UPDATED, userInfo => {
+  console.log('user info updated', userInfo)
+  Store.dispatch({type: SocketActions.USER_INFO_UPDATED, payload: userInfo})
+})
+
+Socket.on(SocketActions.ALL_CLASS_DATA, allClassData => {
+  console.log('inital class data received', allClassData)
+  Store.dispatch({type: SocketActions.ALL_CLASS_DATA, payload: allClassData})
 })
