01cf78493caf9f95c00965a241f449c70a5714d5
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 14:41:19 -0500

    Add temporary object to generate usernames
    Make a user name generator to help create data while we don’t have access to real data

--- ApiServer/socketserver.js
+++ ApiServer/socketserver.js
@@ -3,6 +3,72 @@ const io = require('socket.io')
 //TODO: once we figure out authentication, we need to prevent non-auth'd users from being able to
 //connect to our socket server.
 
+//temporary obj to hold user data
+const nameData = {
+  count: 0,
+  getName() {
+    if (this.count >= this.list.length) {
+      this.count = 1
+      return this.list[0]
+    }
+    const str = this.list[this.count]
+    this.count++
+    return str
+  },
+  list: [
+    'Jazmine Kidney',
+    'Marlys Hannah',
+    'Moriah Treaster',
+    'Golden Bloyd',
+    'Kiana Chartrand',
+    'Willow Brockwell',
+    'Xuan Fronk',
+    'Victoria Mebane',
+    'Tiffaney Wile',
+    'Troy Gervais',
+    'Valda Carriere',
+    'Deborah Loder',
+    'Karl Giddens',
+    'Marguerite Brookes',
+    'Cameron Rushford',
+    'Charlesetta Lundstrom',
+    'Klara Gallman',
+    'Angele Harry',
+    'Norah Pears',
+    'Debora Waymire',
+    'Norine Messerly',
+    'Mark Weingart',
+    'Stephanie Mcginnis',
+    'Roscoe Birdsell',
+    'Santa Staudt',
+    'Jonathon Abram',
+    'Tomas Lagarde',
+    'Renda Strauch',
+    'Carolynn Mullikin',
+    'Elias Scogin',
+    'Edward Elric',
+    'Ty Gaeth',
+    'Imelda Melnick',
+    'Jackie Kuiper',
+    'Stacey Mone',
+    'Elfreda Antonelli',
+    'Elizabeth Soriano',
+    'Kristyn Hultman',
+    'Miles Andre',
+    'Tammera Phaneuf',
+    'Roxane Wirtz',
+    'Vergie Level',
+    'Verna Einhorn',
+    'Nereida Romanowski',
+    'Dorathy Cafferty',
+    'Sean Fadden',
+    'Garrett Bossard',
+    'Alla Montijo',
+    'Randall Reddington',
+    'Arlinda Rent',
+  ]
+}
+
 module.exports = function(server) {
   const socketServer = io(server)
   const connections = []
