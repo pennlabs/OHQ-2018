1cdb2a618cb1c79050e5a4db83a066a140c48fb7
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 15:00:20 -0500

    Remove selected class from socket action types

--- frontend/src/sockets/socketActionTypes.js
+++ frontend/src/sockets/socketActionTypes.js
@@ -13,10 +13,5 @@ export const CLASS_QUEUE_UPDATED = 'CLASS_QUEUE_UPDATED'
 //this is used to update user info, and to send the initial info to the user.
 export const USER_INFO_UPDATED = 'USER_INFO_UPDATED'
 
-//This is used to update which class is selected, NOT to update data on the selected class itself.
-//For the latter, use UPDATE_CLASS.  NOTE: probably don't need this.
-export const UPDATE_SELECTED_CLASS = 'UPDATE_SELECTED_CLASS'
-export const SELECTED_CLASS_UPDATED = 'SELECTED_CLASS_UPDATED'
-
 //this is used to send all the relevant classes when a user first connects
 export const ALL_CLASS_DATA = 'ALL_CLASS_DATA'
