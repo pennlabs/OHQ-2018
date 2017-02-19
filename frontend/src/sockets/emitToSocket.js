9e337433e552aaec6d1d9005d9c5c222b91e9b30
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 14:24:32 -0500

    update emitToSocket file

--- frontend/src/sockets/emitToSocket.js
+++ frontend/src/sockets/emitToSocket.js
@@ -1,9 +1,24 @@
 import Socket from './initSocket'
 import * as SocketActions from './socketActionTypes'
 
-//updateQueue takes a single object, with a `question` key, `location` key,
-//`class` key, and `user` key.  HACK: for now, the `user` information is the socket ID
-//and there is no class information
-export function updateQueue(data) {
-  Socket.emit(SocketActions.UPDATE_QUEUE, {...data, user: Socket.id.slice(0, 6), class: 'classFoo'})
+//updateClass takes an entire class as a parameter.
+export function updateClass(myClass) {
+  Socket.emit(SocketActions.UPDATE_CLASS, myClass)
 }
+
+//updateClassQueue takes an object containing question, location, userInfo, and class properties.
+export function updateClassQueue(data) {
+  Socket.emit(SocketActions.UPDATE_CLASS_QUEUE, data)
+}
+
+//NOTE: probably remove this, since selected class is something only a single user cares about
+//and does not involve modifying any data.
+export function updateSelectedClass(myClass) {
+  Socket.emit(SocketActions.UPDATE_SELECTED_CLASS, myClass)
+}
+
+//NOTE: may not be necessary, updateClass could simply add a class.
+//updateClass takes an entire class as an object.
+// export function addClass(myClass) {
+//   Socket.emit(SocketActions.UPDATE_CLASS, myClass)
+// }
