ea19d810493eea7f1e008273b53104c4b709264d
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 14:26:20 -0500

    Add classes reducer file

---
+++ frontend/src/reducers/ClassesReducer.js
@@ -0,0 +1,30 @@
+//This file holds data for all classes relevant to the user in a single object.
+//the key for each class is the class’ ID.
+
+import {
+  CLASS_QUEUE_UPDATED,
+  ALL_CLASS_DATA
+} from './../sockets/socketActionTypes'
+
+//Classes is a an object made of class class objects,
+//where each class object is identied by a key identical to its id field.
+const defaultState = {
+  0: {
+    queue: [],
+    TAs: [],
+    isActive: true,
+    id: 0,
+    name: 'CIS 110'
+  }
+}
+
+export default function(state = defaultState, action) {
+  switch (action.type) {
+    case CLASS_QUEUE_UPDATED:
+      return {...state, [action.payload.id]: action.payload}
+    case ALL_CLASS_DATA:
+      return action.payload
+    default:
+      return state
+  }
+}
