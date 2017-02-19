2bea52b99281cccc4847dc897c246808aa7ab4bc
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-19 13:52:15 -0500

    update selected class reducer

--- frontend/src/reducers/SelectedClassReducer.js
+++ frontend/src/reducers/SelectedClassReducer.js
@@ -1,6 +1,15 @@
 import { UPDATE_SELECTED_CLASS } from './../actions/ActionTypes'
 
-const defaultState = 'classFoo'
+//Selected class is the class whose information will be given to the ClassPage react component.
+//It is just the information for a single class
+
+const defaultState = {
+  queue: [],
+  TAs: [],
+  isActive: true,
+  id: 0,
+  name: 'classFoo'
+}
 
 export default function(state = defaultState, action) {
   switch (action.type) {
