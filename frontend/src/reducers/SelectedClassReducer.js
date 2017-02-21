eba123ff591ab93b8ec65105c2ee356203ae74ef
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-21 02:05:25 -0500

    change selectedClass to simply be the id number
    Before we had selectedClass contain the entire class object; however, this caused issues because selectedClass needs to update whenever classes update as a whole.  So instead, we will track only the selected class Id, and we will use this id to find the class whose info we care about

--- frontend/src/reducers/SelectedClassReducer.js
+++ frontend/src/reducers/SelectedClassReducer.js
@@ -3,13 +3,7 @@ import { UPDATE_SELECTED_CLASS } from './../actions/ActionTypes'
 //Selected class is the class whose information will be given to the ClassPage react component.
 //It is just the information for a single class
 
-const defaultState = {
-  queue: [],
-  TAs: [],
-  isActive: true,
-  id: 0,
-  name: 'classFoo'
-}
+const defaultState = 0
 
 export default function(state = defaultState, action) {
   switch (action.type) {
