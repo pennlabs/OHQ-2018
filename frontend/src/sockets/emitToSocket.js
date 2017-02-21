c72e0e514d2033f1cac8b874f521faa408810cb4
Aaron Michaels <aaronmichaels@gmail.com>
2017-02-20 23:07:57 -0500

    remove updateSelectedClass socket action

--- frontend/src/sockets/emitToSocket.js
+++ frontend/src/sockets/emitToSocket.js
@@ -11,12 +11,6 @@ export function updateClassQueue(data) {
   Socket.emit(SocketActions.UPDATE_CLASS_QUEUE, data)
 }
 
-//NOTE: probably remove this, since selected class is something only a single user cares about
-//and does not involve modifying any data.
-export function updateSelectedClass(myClass) {
-  Socket.emit(SocketActions.UPDATE_SELECTED_CLASS, myClass)
-}
-
 //NOTE: may not be necessary, updateClass could simply add a class.
 //updateClass takes an entire class as an object.
 // export function addClass(myClass) {
