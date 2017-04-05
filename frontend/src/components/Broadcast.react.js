0bc266634db06d358b5145d4c2f4b3496a9d6490
Aaron Michaels <aaronmichaels@gmail.com>
2017-04-05 18:47:06 -0400

    Broadcast frontend component

---
+++ frontend/src/components/Broadcast.react.js
@@ -0,0 +1,74 @@
+import React, { Component, PropTypes } from 'react'
+import { updateBroadcast } from './../sockets/emitToSocket'
+
+class Broadcast extends Component {
+
+  static propTypes = {
+    broadcastMessage: PropTypes.string,
+    selectedClassId: PropTypes.number,
+    isUserTAForSelectedClass: PropTypes.bool
+  }
+
+  state = {
+    broadcastMessage: ''
+  }
+
+  // Don't think we need to handle a user becoming a TA in the middle of a session
+  componentWillReceiveProps({ broadcastMessage }) {
+    if (broadcastMessage !== this.props.broadcastMessage) {
+      this.setState({ broadcastMessage })
+    }
+  }
+
+  componentWillMount() {
+    if (this.props.broadcastMessage) {
+      this.setState({ broadcastMessage: this.props.broadcastMessage })
+    }
+  }
+
+  onChange = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    this.setState({broadcastMessage: e.target.value})
+  }
+
+  updateBroadcastMessage = () => {
+    updateBroadcast({
+      classId: this.props.selectedClassId,
+      broadcast: this.state.broadcastMessage
+    })
+  }
+
+  renderHeader(string) {
+    return (
+      <div>{string}</div>
+    )
+  }
+
+  renderStudentBroadcast() {
+    return (
+      <div>
+        {this.renderHeader('Announcements')}
+        <div>{this.props.broadcastMessage || 'No Announcements'}</div>
+      </div>
+    )
+  }
+
+  renderTABroadcast() {
+    return (
+      <div>
+        {this.renderHeader('Announcements')}
+        <input value={this.state.broadcastMessage} onChange={this.onChange} />
+        <button onClick={this.updateBroadcastMessage}>Update Announcement</button>
+      </div>
+    )
+  }
+
+  render() {
+    return this.props.isUserTAForSelectedClass
+    ? this.renderTABroadcast()
+    : this.renderStudentBroadcast()
+  }
+}
+
+export default Broadcast
