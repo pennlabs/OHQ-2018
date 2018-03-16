c111cce7ce19a7911055964bae60a1ed38b7e5fc
Aaron Michaels <aaronmichaels@gmail.com>
2018-03-16 04:44:03 -0400

    Remove app and main
    Entrypoint will be determined from routes alone

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,19 +1,12 @@
-//since we are using a single page app architecture, we do not have routes
-//in the traditional sense.  This file, using react-router, mimics routing
-//behavior for our frontend
-
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import App from './components/app.react'
 import SplashPage from './components/SplashPage.react'
-import Main from './components/Main.react'
-import Signin from './components/auth/Signin.react'
+import VerifyClass from './components/VerifyClass.react'
 
 export default (
-  <Route path='/' component={App}>
-    <IndexRoute component={Main} />
-    <Route path='/signin' component={SplashPage}>
-    </Route>
+  <Route path='/' component={({ children }) => children}>
+    <IndexRoute component={SplashPage} />
+    <Route path='/:candidate' component={VerifyClass} />
   </Route>
 )
