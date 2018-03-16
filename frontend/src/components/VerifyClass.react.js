8c0aa7c4df1a04307d055731867d1a967022e209
Aaron Michaels <aaronmichaels@gmail.com>
2018-03-16 04:44:26 -0400

    Add component to determine if class is valid
    Still work in progress

---
+++ frontend/src/components/VerifyClass.react.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import styles from './../../style/Main.less'
+// import ClassPage from './ClassPage.react'
+import { joinClass } from './../sockets/emitToSocket'
+
+// This component acts as a controller.  When a potential path is provided, this component
+// waits to see if a the path is valid, and renders the app according to the three possible states
+// (TA, Student, Invalid)
+class VerifyClass extends Component {
+
+  static propTypes = {
+    // from react-router
+    location: PropTypes.object
+    // from redux
+  }
+
+  state = {
+    pending: true
+  }
+
+  componentDidMount() {
+    joinClass(this.props.location.pathname.slice(1))
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        path is {this.props.location.pathname}
+        <br />
+        status is {this.props.joinClassStatus}
+        {/* <ClassPage /> */}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ joinClassStatus }) {
+  return { joinClassStatus }
+}
+
+export default connect(mapStateToProps)(VerifyClass)
