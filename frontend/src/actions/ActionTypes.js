b717bf35e3e6a05e52f6106f34428693c29b7376
Aaron Michaels <aaronmichaels@gmail.com>
2018-03-16 04:41:45 -0400

    Update local actions

--- frontend/src/actions/ActionTypes.js
+++ frontend/src/actions/ActionTypes.js
@@ -1,4 +1,6 @@
-export const AUTH_USER = 'AUTH_USER'
-export const UNAUTH_USER = 'UNAUTH_USER'
-export const AUTH_ERROR = 'AUTH_ERROR'
-export const UPDATE_SELECTED_CLASS = 'UPDATE_SELECTED_CLASS'
+// legacy
+// export const AUTH_USER = 'AUTH_USER'
+// export const UNAUTH_USER = 'UNAUTH_USER'
+// export const AUTH_ERROR = 'AUTH_ERROR'
+
+export const USER_INFO_UPDATED = 'USER_INFO_UPDATED'
--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -3,50 +3,55 @@
 // By contrast, socket actions interact with the backend
 // via websockets.
 
-import axios from 'axios'
-import { browserHistory } from 'react-router'
-import * as Actions from './ActionTypes'
-import { ROOT_URL } from './../../constants'
+import { USER_INFO_UPDATED } from './ActionTypes'
 
-const rootUrl = process.env.ROOT_URL || ROOT_URL
-
-export function authError(error) {
+// TODO: maybe use redux-thunk to set localstorage instead
+// of putting that logic in the reducer
+export function updateUserInfo({ firstName, lastName, id }) {
   return {
-    type: Actions.AUTH_ERROR,
-    payload: error,
+    type: USER_INFO_UPDATED,
+    payload: { firstName, lastName, id }
   }
 }
 
-export function updateSelectedClass(classId) {
-  return {
-    type: Actions.UPDATE_SELECTED_CLASS,
-    payload: classId
-  }
-}
+// function authError(error) {
+//   return {
+//     type: Actions.AUTH_ERROR,
+//     payload: error,
+//   }
+// }
 
-export function signinUser({ email, password }) {
+// function updateSelectedClass(classId) {
+//   return {
+//     type: Actions.UPDATE_SELECTED_CLASS,
+//     payload: classId
+//   }
+// }
 
-  //reduxThunk allows us to return a function instead of a raw object
-  //allows us to decide when to dispatch our object, and perform logic prior to submit
-  //also allows us to dispatch many different actions!
-  return function(dispatch) {
 
-    //submit email/pass to server
-    axios.post(`${rootUrl}/signin`, { email, password })
-      .then(res => {
-        //if request is good, update state to indicate user is auth'd
-        dispatch({ type: Actions.AUTH_USER })
+// function signinUser({ email, password }) {
+//   const rootUrl = process.env.ROOT_URL || ROOT_URL
+//   //reduxThunk allows us to return a function instead of a raw object
+//   //allows us to decide when to dispatch our object, and perform logic prior to submit
+//   //also allows us to dispatch many different actions!
+//   return function(dispatch) {
 
-        //save JWT token in LocalStorage (available on window-scope)
-        localStorage.setItem('token', res.data.token)
+//     //submit email/pass to server
+//     axios.post(`${rootUrl}/signin`, { email, password })
+//       .then(res => {
+//         //if request is good, update state to indicate user is auth'd
+//         dispatch({ type: Actions.AUTH_USER })
 
-        //re-direct user from signin route to feature route
-        browserHistory.push('/feature')
-      })
-      .catch(err => {
-        //if request is bad
-        //show the user an error
-        dispatch(authError('Bad login info' + err))
-      })
-  }
-}
+//         //save JWT token in LocalStorage (available on window-scope)
+//         localStorage.setItem('token', res.data.token)
+
+//         //re-direct user from signin route to feature route
+//         browserHistory.push('/feature')
+//       })
+//       .catch(err => {
+//         //if request is bad
+//         //show the user an error
+//         dispatch(authError('Bad login info' + err))
+//       })
+//   }
+// }
