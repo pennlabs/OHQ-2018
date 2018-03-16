3388a1fb1f98c6185d32264ac61db672aeb89e91
Aaron Michaels <aaronmichaels@gmail.com>
2018-03-16 04:42:51 -0400

    update existing reducers

--- frontend/src/reducers/UserInfoReducer.js
+++ frontend/src/reducers/UserInfoReducer.js
@@ -1,14 +1,34 @@
-import { SocketActions } from './../../../shared'
-import { UserInfo } from './../../../shared/'
+import { SocketActions, UserInfo } from './../../../shared'
+import uuid from 'uuid/v4'
+
+// User information is stored client sie in localstorage.
+// Thus anytime user info is updated locally, we store it here.
+// Note that id should never be set anywhere apart from
+// the initial check here.
+
+//TODO: may want to use redux-thunk to clean this up
+
 const { USER_INFO_UPDATED } = SocketActions
 
-const defaultState = new UserInfo(null, null, null)
+// set local storage user information if it doesn't exist
+if (localStorage.getItem('pennlabsohq') == null) {
+  localStorage.setItem('pennlabsohq', JSON.stringify({
+    id: uuid(),
+    firstName: '',
+    lastName: ''
+  }))
+}
+
+const defaultState = JSON.parse(localStorage.getItem('pennlabsohq '))
 
 export default function(state = defaultState, action) {
   switch (action.type) {
-  case USER_INFO_UPDATED:
-    return action.payload
-  default:
-    return state
+    case USER_INFO_UPDATED: { // new block for scoping
+      const newState = {...state, ...action.payload}
+      localStorage.setItem('pennlabsohq', JSON.stringify(newState))
+      return newState
+    }
+    default:
+      return state
   }
 }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,24 +1,24 @@
 //this file is the root reducer, and as such holds the global
 //application (redux) state.
 
-//Note that we have two kinds of actions.  Those that emit to a websocket
-//need to be handled differently than those that don't, so the action types that reducers handle
-//are split across two files: src/actions/ActionTypes.js for frontend actions
+// Note that we have two kinds of actions.  Those that emit to a websocket
+// need to be handled differently than those that don't, so the action types that reducers handle
+// are split across two files: src/actions/ActionTypes.js for frontend actions
 // and SocketActions in the shared folder for websocket actions which
 // need to be handled by both the frontend and the backend
 
 import { combineReducers } from 'redux'
 
-import authReducer from './AuthReducer'
-import selectedClassReducer from './SelectedClassReducer'
 import userInfoReducer from './UserInfoReducer'
-import classesReducer from './ClassesReducer'
+import classReducer from './ClassReducer'
+import removedFromQueueReducer from './RemovedFromQueueReducer'
+import linksReducer from './LinksReducer'
 
 const rootReducer = combineReducers({
-  auth: authReducer,
   userInfo: userInfoReducer,
-  classes: classesReducer,
-  selectedClass: selectedClassReducer,
+  classInfo: classReducer,
+  classLinks: linksReducer,
+  userJustRemovedFromQueue: removedFromQueueReducer,
 })
 
 export default rootReducer
