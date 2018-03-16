347d9938c4f013b68a0d85811ecefd6fbe400c46
Aaron Michaels <aaronmichaels@gmail.com>
2018-03-16 03:20:31 -0400

    add clojure server skeleton

---
+++ clojure-server/ring-app/project.clj
@@ -0,0 +1,12 @@
+(defproject ring-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [environ "1.1.0"]]
+  :main ring-app.core)
---
+++ clojure-server/ring-app/src/ring_app/core.clj
@@ -0,0 +1,50 @@
+(ns ring-app.core
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :refer [response]]
+            [ring.middleware.file :as file]
+            [org.httpkit.server :refer [run-server]]
+            [compojure.core :refer [defroutes GET context ANY]]
+            [compojure.route :as route]
+            [clojure.data.json :as json]
+            [environ.core :as environ]))
+
+(defn handler [request-map]
+  (response
+    (str "<html><body> all data: "
+         (:remote-addr request-map)
+         "</body></html>")))
+
+(defn handler2 [request-map]
+  (response
+    (str "<html><body> all dabbbaa: "
+         (:remote-addr request-map)
+         "</body></html>")))
+
+(defn file-handler [request-map]
+
+  )
+
+(defroutes routes
+           (GET "/" [] handler)
+           (GET "/ws" [] handler2)
+           (GET "/:id" [] handler)
+           (route/files "static")
+           )
+
+(defn -main []
+  (run-server
+    ; due to implementation details we pass a var here
+    ; to allow our handler to be dynamically redefined
+    #'routes
+    {:port (or (Integer/parseInt (environ/env :port)) 3000)}))
+
+
+(def counter (atom 0))
+
+(defn up-counter [n] (+ n 1))
+
+(defn tick [] (Thread/sleep 1000) (println (swap! counter up-counter)))
+
+(defn execute []
+  (future (dorun (repeatedly tick))))
