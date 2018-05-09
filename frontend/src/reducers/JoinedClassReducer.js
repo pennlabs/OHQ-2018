e652d4f63d8a6e4bba5fc12910e35c7da29da4ab
Aaron Michaels <aaronmichaels@gmail.com>
2018-05-09 19:23:46 -0400

    update joinedclassreducer

--- frontend/src/reducers/JoinedClassReducer.js
+++ frontend/src/reducers/JoinedClassReducer.js
@@ -1,5 +1,20 @@
 import { SocketActions } from './../../../shared'
 
+const {
+  CLASS_JOINED_FAILURE,
+  CLASS_JOINED_STUDENT,
+  CLASS_JOINED_TA
+} = SocketActions
+
 const defaultState = null
+
 export default function(state = defaultState, action) {
+  switch (action.type) {
+    case CLASS_JOINED_FAILURE:
+    case CLASS_JOINED_STUDENT:
+    case CLASS_JOINED_TA:
+      return action.type
+    default:
+      return state
+  }
 }
